387b307c1a90e80c8480c81313d67b7256d732c1
itsfeas <amf.fiaz@gmail.com>
2023-12-05 17:29:50 -0700

    remove pos knowledge from piece

--- piece.py
+++ piece.py
@@ -1,38 +1,22 @@
 class Piece:
-	_pos = (None, None)    # coordinate (x, y)
 	_color = None          # color of piece. can be 1 (black) or 2 (white)
 	_isKing = False        # is piece a king
 	
-	def __init__(self, x: int, y: int, color: int):
-		self._pos = (x, y)
+	def __init__(self, color: int):
 		self._color = color
 
 	def __str__(self):
+		s = "+" if self._isKing else ""
+		return s + "W" if self._color == 2 else "B"
+	
+	def __repr__(self):
 		return f"[color: {self._color}, isKing: {self._color}, pos: {self._pos}]"
-	
-	def get_pos(self):
-		return self._pos
-	
-	def get_x(self):
-		return self._pos[0]
-	
-	def get_y(self):
-		return self._pos[1]
 	
 	def get_color(self):
 		return self._color
 	
 	def get_isKing(self):
 		return self._isKing
-
-	def set_x(self, x: int):
-		self._pos[0] = x
-
-	def set_y(self, y: int):
-		self._pos[1] = y
-
-	def set_pos(self, x: int, y: int):
-		self._pos = (x, y)
 
 	def set_color(self, color: int):
 		self._color = color
