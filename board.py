936245af9ffe93e01951f9e2c1992c543420a366
itsfeas <amf.fiaz@gmail.com>
2023-12-05 23:11:27 -0700

    fix bad successors method

--- board.py
+++ board.py
@@ -7,6 +7,8 @@ class CheckersBoard:
 	
 	def __init__(self):
 		self._board = [[None]*8 for i in range(8)]
+
+	def initial_board(self):
 		for i in range(3):
 			for j in range(8):
 				if (i + j) % 2 == 0:
