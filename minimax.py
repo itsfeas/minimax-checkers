8d293e5c2abc192cdc1a079fdc2cba61f1a7f510
itsfeas <amf.fiaz@gmail.com>
2023-12-05 23:11:43 -0700

    init minimax

--- minimax.py
+++ minimax.py
@@ -1,13 +1,21 @@
 from board import CheckersBoard
 
 class CheckersMiniMax:
-	_board: CheckersBoard           # board
-	
-	def __init__(self):
-		self._board = CheckersBoard()
+	def get_move(self, board: CheckersBoard, d: int) -> int:
+		pass
 
-	def __str__(self):
-		return self._board
-	
-	def __repr__(self):
-		return self._board
+	# def __traverse(self, board: CheckersBoard, d: int):
+	# 	for 
+
+	def __get_all_moves(self, board: CheckersBoard, color: int, d: int):
+		return board.generate_all_sucessors(color)
+		
+
+if __name__ == "__main__":
+	board = CheckersBoard()
+	board.initial_board()
+	print(board)
+	s = board.generate_all_sucessors(1)[0]
+	print(s)
+	s = s.generate_all_sucessors(2)[0]
+	print(s)
